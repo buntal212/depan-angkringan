bf71945a5ecfdf61f5b4f9b9446c2acd95496a94
kentangkotak <ariezlnd69@gmail.com>
2026-05-17 22:28:12 +0700

    sasa

--- quasar.config.js
+++ quasar.config.js
@@ -78,7 +78,11 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      config: {
+        brand: {
+          primary: '#000000',
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
